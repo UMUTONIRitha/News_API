4f67026b46a826c08baa90cd999b3b99e5abc588
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-21 11:12:17 +0200

    add variables

--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
 from flask import render_template
 from app import app
+from .request import my_news
+
 # Views
 @app.route('/')
 def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    myNewsdata = my_news()
+    print(myNewsdata)
+    return render_template('index.html',data = myNewsdata)
