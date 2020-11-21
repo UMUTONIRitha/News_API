37c5efda090c06e2404683c16c9e6f068120c542
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-21 23:42:51 +0200

    create source class

---
+++ app/sources.py
@@ -0,0 +1,10 @@
+class Sources:
+    
+    def __init__ (self,category,name,description,url):
+
+   
+        self.news_category = category
+        self.news_name = name
+        self.news_description = description
+        self.news_url = url
+       
