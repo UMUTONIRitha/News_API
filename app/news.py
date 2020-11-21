857ac46bd64ea2e9d2ce43ef82ea2796225eba65
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-21 16:09:44 +0200

    add class sources

--- app/news.py
+++ app/news.py
@@ -1,10 +1,28 @@
-class News:
-    def __init__ (self,source, author,title,description,publishedAt,url,poster):
-        
-        self.news_source = source
+class News_sources:
+    '''
+    class of news source to define news object
+    '''
+
+    def __init__ (self,id,name,description,url,category):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+
+
+
+
+
+
+
+class News_Articles:
+    def __init__ (self,id,author,title,description,publishedAt,url,urlToImage):
+
+        self.news_id = id
         self.news_author = author
         self.news_title = title
         self.news_description = description
         self.news_publishedAt = publishedAt
         self.news_url = url
-        self.news_poster = poster
+        self.news_urlToImage = urlToImage
