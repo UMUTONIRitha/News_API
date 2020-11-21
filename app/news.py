b22406118b10dfc1ba90b714b3b415bead9e6599
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-21 22:51:44 +0200

    delete class source

--- app/news.py
+++ app/news.py
@@ -1,28 +1,12 @@
-class News_sources:
-    '''
-    class of news source to define news object
-    '''
+class News:
+    
+    def __init__ (self, author,title,description,publishedAt,url,urlToImage):
 
-    def __init__ (self,id,name,description,url,category):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.url = url
-        self.category = category
-
-
-
-
-
-
-
-class News_Articles:
-    def __init__ (self,id,author,title,description,publishedAt,url,urlToImage):
-
-        self.news_id = id
+   
         self.news_author = author
         self.news_title = title
         self.news_description = description
         self.news_publishedAt = publishedAt
         self.news_url = url
         self.news_urlToImage = urlToImage
+     
