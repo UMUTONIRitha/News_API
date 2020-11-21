177f38a1c8b35cd72597c2f5bc03293df5470757
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-22 00:09:23 +0200

    add route to index and source

--- app/views.py
+++ app/views.py
@@ -1,11 +1,21 @@
 from flask import render_template,request,redirect,url_for
 from app import app
-from .request import my_news
+from .request import my_news,my_sources
 # Views
 @app.route('/')
+def sources():
+    '''
+    View root page function that returns the source page and its data
+    '''
+    mySourcesData = my_sources()
+    print(mySourcesData)
+    return render_template('sources.html',sourceData=mySourcesData)
+
+
+@app.route('/articles')
 def index():
     '''
-    View root page function that returns the index page and its data
+    View second page function that returns the index page and its data
     '''
     myNewsData = my_news()
     print(myNewsData)
