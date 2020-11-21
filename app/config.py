2c1959d82d5a020f053f321aed2dfd22b88d250b
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-21 23:42:04 +0200

    add sources link

--- app/config.py
+++ app/config.py
@@ -1,9 +1,10 @@
 class Config:
     NEWS_API_LINK = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey={}'
-
+    SOURCES_API_LINK = 'https://newsapi.org/v2/sources?apiKey={}'
 class ProdConfig:
     pass
 
 class DevConfig:
     NEWS_API_LINK = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey={}'
+    SOURCES_API_LINK = 'https://newsapi.org/v2/sources?apiKey={}'
     DEBUG = True
