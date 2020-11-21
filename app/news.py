4b29eeac93d679b329ce60714ce20b03796eacf7
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-21 09:39:22 +0200

    create class and add its properties

---
+++ app/news.py
@@ -0,0 +1,9 @@
+class News:
+    def__init__(self,source, author,title,description,publishedAt,url,poster):
+    news_source = source
+    news_author = author
+    news_title = title
+    news_description = description
+    news_publishedAt = publishedAt
+    news_url = url
+    news_poster = poster
