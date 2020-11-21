cfee69f9639f9ee51d0cb372102657eb2658eb8e
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-21 16:08:35 +0200

    add changes

--- app/config.py
+++ app/config.py
@@ -1,10 +1,12 @@
 
 class Config:
-    NEWS_API_LINK = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey={}'
-
+    NEWS_SOURCES_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
+    NEWS_ARTICLES_URL= 'https://newsapi.org/v2/everything?q=bitcoin&apiKey={}'
+   
+    # NEWS_API_KEY = 'f747d2632bd44a26aa624f00083b9df3'
 class ProdConfig:
     pass
 
 class DevConfig:
-    NEWS_API_LINK = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey={}'
+    
     DEBUG = True
