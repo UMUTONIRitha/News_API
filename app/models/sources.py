dec7cb67bb7d791ff7ae799064a409a78ae50a5d
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-23 11:12:41 +0200

    add changes

---
+++ app/models/sources.py
@@ -0,0 +1,11 @@
+class Source:
+    '''
+    source class to define sourceObjects
+    '''
+
+    def __init__(self,id,name,category,description,url):
+        self.id =id
+        self.name = name
+        self.category = category
+        self.description = description
+        self.url = url
