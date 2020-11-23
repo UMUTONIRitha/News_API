39aeb0e4426ffc3e57083e8332713ed2347e2b3c
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-23 10:22:05 +0200

    add imports and correct errors

--- app/views.py
+++ app/views.py
@@ -1,22 +1,34 @@
 from flask import render_template,request,redirect,url_for
 from app import app
-from .request import my_news,my_sources
+from .request import get_sources, get_articles
+from .models import sources,articles
+
 # Views
 @app.route('/')
-def sources():
-    '''
-    View root page function that returns the source page and its data
-    '''
-    mySourcesData = my_sources()
-    print(mySourcesData)
-    return render_template('sources.html',sourceData=mySourcesData)
-
-
-@app.route('/articles')
 def index():
+
     '''
-    View second page function that returns the index page and its data
+    View root page function that returns the index page and its data
     '''
-    myNewsData = my_news()
-    print(myNewsData)
-    return render_template('index.html',data=myNewsData)
+
+    # Getting cateory news
+
+    general_sources = get_sources('general')
+    technology_sources = get_sources('technology')
+    entertainment_sources = get_sources('entertainment')
+    business_sources = get_sources('business')
+    health_sources = get_sources('health')
+
+    title = 'Home - Welcome to The best category news Website Online'
+    return render_template('index.html', title = title,general = general_sources,technology = technology_sources,entertainment = entertainment_sources,business = business_sources,health = health_sources)
+
+
+@app.route('/sources/<id>')
+def articles(id):
+    '''
+    View article page function that returns the article details page and its data
+    '''
+    articles = get_articles(id)
+    title = f'You are viewing {id}'
+    
+    return render_template('article.html',title = title,articles = articles)
