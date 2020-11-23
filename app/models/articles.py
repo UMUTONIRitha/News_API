854f446823a87595d0984ea07ad9d75b88fa9f92
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-23 11:12:20 +0200

    add changes

---
+++ app/models/articles.py
@@ -0,0 +1,11 @@
+class Artcle:
+    
+    def __init__ (self,title,description,url,urlToImage,publishedAt):
+
+   
+      
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
