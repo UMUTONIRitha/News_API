dece10b9c5054e3b90f8254489ac60761eebf4d5
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-23 10:28:46 +0200

    run test

--- app/source_test.py
+++ app/source_test.py
@@ -1,19 +1,20 @@
 import unittest
-from sources import Sources
+from models import source
+Source = source.Source
 
-class SourcesTest(unittest.TestCase):
+class SourceTest(unittest.TestCase):
     '''
-    Test Class to test the behaviour of the Sources class
+    Test Class to test the behaviour of the Source class
     '''
 
     def setUp(self):
         '''
         Set up method that will run before every Test
         '''
-        self.new_sources= Sources('general','IGIHE','Amakuru ashyushye wayasanga ku Igihe','https://image.tmdb.org/t/p/w500/khsjha27hbs')
+        self.new_source = Source('IGIHE','Ritha','general','A thrilling new Python Series','https://abcnews.go.com')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_sources,Sources))
+        self.assertTrue(isinstance(self.new_source,Source))
 
 
 if __name__ == '__main__':
